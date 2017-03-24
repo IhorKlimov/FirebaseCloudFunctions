abe5bddb0746a23019731d98d35d8471c24c834f
IhorKlimov <iklimov6@gmail.com>
2017-03-24 10:02:35 +0100

    trigger

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const GeoFire = require('geofire');
 admin.initializeApp(functions.config().firebase);
 const geoFire = new GeoFire(admin.database().ref('/geofire'));
-
+const gcs = require('@google-cloud/storage')().bucket('hello');
 
 exports.addMessage = functions.https.onRequest((req, res) => {
   const original = req.query.text;
@@ -12,13 +12,26 @@ exports.addMessage = functions.https.onRequest((req, res) => {
   });
 });
 
-exports.addUser = functions.https.onRequest((req, res) => {
-  const user = req.body;
-  var newUser = admin.database().ref('/users').push();
-  newUser.set(user).then(snapshot => {
-    var location = [user.latitude, user.longitude];
-    geoFire.set(newUser.key, location).then(function() {
-      res.send(200, "ok");
+// API approach (extra work on Android for setting up Retrofit)
+// exports.addUser = functions.https.onRequest((req, res) => {
+//   const user = req.body;
+//   var newUser = admin.database().ref('/users').push();
+//   newUser.set(user).then(snapshot => {
+//     var location = [user.latitude, user.longitude];
+//     geoFire.set(newUser.key, location).then(function() {
+//       res.send(200, "ok");
+//     });
+//   });
+// });
+
+// Trigger approach (no extra work on Android)
+exports.saveUser = functions.database.ref('/users/{pushId}')
+    .onWrite(event => {
+      const user = event.data.val();
+      var latitude = event.data.child('latitude');
+      var longitude = event.data.child('longitude');
+      if (latitude.changed() || longitude.changed()) {
+        const location = [user.latitude, user.longitude];
+        return geoFire.set(event.params.pushId, location);
+      }
     });
-  });
-});
