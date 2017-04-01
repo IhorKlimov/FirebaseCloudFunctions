024774c303ee6baf60e48fc0b55622cfea040f19
IhorKlimov <iklimov6@gmail.com>
2017-04-01 18:25:43 +0200

    added notifications

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const gcs = require('@google-cloud/storage')().bucket('hello');
 
 exports.addMessage = functions.https.onRequest((req, res) => {
   const original = req.query.text;
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
+  admin.database().ref('/messages').push({ original: original }).then(snapshot => {
     res.redirect(303, snapshot.ref);
   });
 });
@@ -26,12 +26,57 @@ exports.addMessage = functions.https.onRequest((req, res) => {
 
 // Trigger approach (no extra work on Android)
 exports.saveUser = functions.database.ref('/users/{pushId}')
-    .onWrite(event => {
-      const user = event.data.val();
-      var latitude = event.data.child('latitude');
-      var longitude = event.data.child('longitude');
-      if (latitude.changed() || longitude.changed()) {
-        const location = [user.latitude, user.longitude];
-        return geoFire.set(event.params.pushId, location);
+  .onWrite(event => {
+    const user = event.data.val();
+    var latitude = event.data.child('latitude');
+    var longitude = event.data.child('longitude');
+    if (latitude.changed() || longitude.changed()) {
+      const location = [user.latitude, user.longitude];
+      return geoFire.set(event.params.pushId, location);
+    }
+  });
+
+exports.sendNotification = functions.https.onRequest((req, res) => {
+  const to = req.query.to;
+  const title = req.query.title;
+  const body = req.query.body;
+
+  var payload;
+  if (body != undefined && body !== '') {
+    payload = {
+      notification: {
+        title: title,
+        body: body
       }
-    });
+    };
+  } else {
+    payload = {
+      notification: {
+        title: title
+      }
+    };
+  }
+
+  var options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24
+  };
+
+  if (to == 'all') {
+    admin.messaging().sendToTopic(to, payload, options)
+      .then(function (response) {
+        res.send(200, 'ok');
+      })
+      .catch(function (error) {
+        res.send(200, 'failed');
+      });
+  } else {
+    admin.messaging().sendToDevice(to, payload, options)
+      .then(function (response) {
+        res.send(200, 'ok');
+      })
+      .catch(function (error) {
+        res.send(200, 'failed');
+      });
+  }
+});
